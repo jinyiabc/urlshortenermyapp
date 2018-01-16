7a0390f5afe8e159be99200308b4816f4c25354f
Glitch (urlshortenermyapp) <none>
2018-01-16 16:21:03 +0000

    👆🙉 Updated with Glitch

--- server.js
+++ server.js
@@ -4,13 +4,11 @@
 // init project
 var express = require('express');
 var app = express();
-// var ids = require('short-id');
+var mongo = require('mongodb').MongoClient
+var ObjectId = require('mongodb').ObjectID;
+// Log in database with dbuser:dbpw database
+var url = 'mongodb://test:1234@ds255767.mlab.com:55767/freecodecamp'
 
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -18,33 +16,74 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// app.get("/views", function (request, response) {
-//   console.log(JSON.stringfy(request.url))
-//   // response.send(dreams);
-// });
 
+app.use(function (req ,res,next) {
+console.log(req.url.slice(1) )
+var result
+ mongo.connect(url, function(err, client) {
+  if (err) throw err
+  var db = client.db('freecodecamp')
+  var collection = db.collection('urlshorten')  
+  collection.find( { shorten_url: "https://urlshortenermyapp.glitch.me/"+req.url.slice(1) } )
+            .toArray(function(err, docs) {
+                     if (err) throw err
+                     client.close()
+                     if(docs.length>0){
+                       var new_url = docs[0]["original_url"]    // www.google.com
+                        result = {
+                        orginal_url:docs[0]["original_url"],
+                        shorten_url:docs[0]["shorten_url"]
+                        }
+                        // console.log(result)
+                       // res.redirect(docs[0]["original_url"])
+                       res.redirect(301, docs[0]["original_url"]);
 
-app.use(function (req ,res) {
+//                         res.writeHead(200, { 'Content-Type': 'application/json' })
+//                         res.end(JSON.stringify(result))
+                        
+                     } else {
+                       next()
+                     }
+                     // client.close()
+                     })
+
+})
+
   
-    // console.log(JSON.stringify(req.url))   // new/https://www.google.com
+})
+
+ app.use(function (req ,res) {
 
     var original_url = JSON.stringify(req.url).slice(6,-1)
-    console.log(original_url)  // https://www.google.com
+    // console.log(original_url)  // https://www.google.com
+//======================================================================================
+// Connecet mLab datebase.
+  var id = ObjectId().getTimestamp().getTime()
+var doc = {
+    _id: id,
+    original_url: original_url,
+    shorten_url: "https://urlshortenermyapp.glitch.me/"+id
+  }
+mongo.connect(url, function(err, client) {
+  if (err) throw err
+  var db = client.db('freecodecamp')
+  var collection = db.collection('urlshorten')
+  collection.insert(doc, function(err,data){
+    if(err) throw err
+    console.log(JSON.stringify(doc))
+    client.close()
+  })
+  
+  collection.createIndex({original_url:1,shorten_url:1,_id:1})
+})
 
-    var result = {
-           original_url:original_url
-    }
 
+  
+    var result =  {
+    original_url: original_url,
+    shorten_url: "https://urlshortenermyapp.glitch.me/"+id
+  } 
 
-        
-//              if (decodeURI(date) != date){
-//                  time = new Date(decodeURI(date))
-//                  result = parsetime(time) 
-
-//                } else if(decodeURI(date) == date){
-//                  time = new Date(Number(date))
-//                  result = parsetime(time)   
-              // }
     
       if (result) {
         res.writeHead(200, { 'Content-Type': 'application/json' })
